35f45b7e9ccc12e4a06c325ab47de546a6532af2
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-09-19 16:25:53 +0000

    eneiwasnj

--- Madlib.java
+++ Madlib.java
@@ -9,15 +9,30 @@ public class Madlib
     
     // prompts the user so they know when 
     // to enter keyboard input within a program
-    System.out.println("Enter a number: ");
+    System.out.println("Gimmie a noun: ");
     
     // nextLine method returns all keyboard input
     // until the user presses the Return or Enter key
-    String userInput = sc.nextLine();
-    
+    String nounI = sc.nextLine();
+
+    System.out.println("Gimmie another noun: ");
+    String nounII = sc.nextLine();
+
+
+    System.out.println("Gimmie a name: ");
+    String dudeI = sc.nextLine();
+
+    System.out.println("Gimmie another name: ");
+    String dudeII = sc.nextLine();
+
+
+    System.out.println("Gimmie a verb: ");
+    String verbI = sc.nextLine();
+
+    //====================================================================//
     // after collecting input, you can treat user input
     // as a normal String value
-    System.out.println("Your value was: " + userInput);
+    System.out.println("Your value was: " + nounI);
     
   }
 }
