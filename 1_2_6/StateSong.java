410c485f28c4a5eb40de8337ae98bdd253e55ce1
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-09-19 16:58:52 +0000

    made this monstrosity

--- 1_2_6/StateSong.java
+++ 1_2_6/StateSong.java
@@ -1,5 +1,23 @@
 public class StateSong {
     public static void main(String[] args) {
-        
+        String chorus = new String("\"When the snow crowned Golden Sierras /n Keep their watch o'er the valleys bloom, \n It is there I would be in our land by the sea, /n Every breeze bearing rich perfume. \n It is here nature gives of her rarest. It is Home Sweet Home to me, /n And I know when I die I shall breathe my last sigh /n For my sunny California.");
+        String verseI   = "I love you, California, you're the greatest state of all. \n I love you in the winter, summer, spring and in the fall. \\n I love your fertile valleys; your dear mountains I adore. \n I love your grand old ocean and I love her rugged shore.";
+        String verseII  = "I love your red-wood forests, love your fields of yellow grain. \n I love your summer breezes and I love your winter rain. \n I love you, land of flowers; land of honey, fruit and wine. /n I love you, California; you have won this heart of mine.";
+        String verseIIIFirstHalf  = "I love your old gray Missions, love your vineyards stretching far./n I love you, California, with your Golden Gate ajar.\n I love your pu";
+        String verseIIISecondHalf = "rple sun-sets, love your skies of azure blue. \n I love you, California; I just can't help loving you.";
+        String verseIV  = "I love you, Catalina, you are very dear to me. \n I love you, Tamalpais" + "(OMG MARIN COUNTY REFERENCE!!!?!!?!?!1!!)"+", and I love Yosemite./n I love you, Land of Sunshine, half your beauties are untold. \n I loved you in my childhood, and I'll love you when I'm old.\"";
+        int randomInterjectedNumber = 98745678;
+    
+        for (int i = 0; i < 10; i++) {
+            System.out.print(randomInterjectedNumber);
+        }
+            System.out.println(verseI);
+            System.out.println(chorus);
+            System.out.println(verseII);
+            System.out.println(verseIIIFirstHalf + randomInterjectedNumber + verseIIISecondHalf);
+            System.out.println(verseIV);
+    
+
+        //THIS IS THE WORST XD
     }
 }
