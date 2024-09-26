47ecda92e0af219774154ff1573ae2db4088865c
Il-Porco-Rosso <144718841+Il-Porco-Rosso@users.noreply.github.com>
2024-09-26 17:00:40 +0000

    edgtolxgkujgv ghyb,k

--- Madlib/Madlib.java
+++ Madlib/Madlib.java
@@ -7,44 +7,88 @@ public class Madlib
     // creates a scanner to process terminal input
     Scanner sc = new Scanner(System.in);
     
+    String dumbwayofdoingmadlibstuffispentsomuchtimeintotheotheroneeeeeee = "In the downtown of <city name>, there lived a once famous celebrity. <proper noun> was their name, and they ran a <food dish> kitchen, giving free <food dish> to the <name of a type of people> of <city name>. In all this goodness, though, <name> had a dark side, because they was in cahoots with the <name of an organization>. Every <number> days, under the cover of the night, <name> would send out goons <verb (infinitive)> <type of building> and take <noun> and <another noun>! Not only that, whenever <name> found out there was a <type of occupation> that <\"-ed\" verb> with business, he would take the <type of occupation> and <verb> him into the street to be <another \"-ed\" verb>. When the celebrity finally got <one more \"-ed\" verb> for <type of crime>, he was sent to <place>, for <another number> years. At least the <food dish> was good though.";
+
+    for (int i = 0; i < dumbwayofdoingmadlibstuffispentsomuchtimeintotheotheroneeeeeee.length(); i++) {
+        if (dumbwayofdoingmadlibstuffispentsomuchtimeintotheotheroneeeeeee.substring(i, (i+2)) = "<") {
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
     // prompts the user so they know when 
     // to enter keyboard input within a program
-    System.out.println("Gimmie a noun: ");
-    
-    // nextLine method returns all keyboard input
-    // until the user presses the Return or Enter key
-    String nounI = sc.nextLine();
+    System.out.println("Gimmie a food dish of some kind: ");
+    String food = sc.nextLine();
 
-    System.out.println("Gimmie another noun: ");
-    String nounII = sc.nextLine();
+    System.out.println("Gimmie the name of a place: ");
+    String place = sc.nextLine();
 
 
     System.out.println("Gimmie the name of a city: ");
     String city = sc.nextLine();
 
     System.out.println("Gimmie a name of a person: ");
-    String dudeI = sc.nextLine();
+    String dude = sc.nextLine();
 
-    System.out.println("Gimmie another name: ");
-    String dudeII = sc.nextLine();
+    System.out.println("Gimmie the name of an organization (full title): ");
+    String gang = sc.nextLine();
 
 
-    System.out.println("Gimmie a verb (in the infinitive, ex: \"to do\", plz dont do that): ");
-    String verbI = sc.nextLine();
+    System.out.println("Gimmie a verb (past tense \"-ed verb\"): ");
+    String edverbI = sc.nextLine();
 
-    System.out.println("Gimmie another verb (in the infinitive): ");
-    String verbII = sc.nextLine();
+    System.out.println("Gimmie another verb (past tense \"-ed verb\"): ");
+    String edverbII = sc.nextLine();
+
+    System.out.println("Gimmie one more verb (past tense \"-ed verb\"): ");
+    String edverbIII = sc.nextLine();
+
+    System.out.println("Gimmie another verb (infinitive \"to\" verb): ");
+    String toverbI = sc.nextLine();
+
+    System.out.println("Gimmie another verb (infinitive \"to\" verb): ");
+    String toverbII = sc.nextLine();
 
     System.out.println("Gimmie an adverb: ");
     String adverbI = sc.nextLine();
     //====================================================================//
     //============ARRAY=THING====================================================//
-    String[] listofmodifyerthingies = {nounI, nounII, city, dudeI, dudeII, verbI, verbII, adverbI};
+    String[] beanbag = {food, place, city, dude, gang, edverbI, edverbII, edverbIII, toverbI, toverbII, adverbI};
     //===========================================================================//
-    for (int i = 0; i < listofmodifyerthingies.length; i++) {
-        System.out.println("Testing: " + listofmodifyerthingies[i]);
+    for (int i = 0; i < beanbag.length; i++) {
+        System.out.println("Testing: " + beanbag[i]);
     }
-    
+  */  
     
   }
 }
